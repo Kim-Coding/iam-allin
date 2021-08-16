695490a02a70f4a0ad0ceb4ff42a9a9cbf868ec6
Kim-Coding <ptibabmgo@gmail.com>
2021-08-16 18:42:35 +0900

    동행복권 api를 통해 번호 불러오는 알고리즘으로 수정

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render
-from django.db.models import Max
-from .models import numlists
 from makenum import makenum
 from django.views.decorators.csrf import csrf_exempt
+from bs4 import BeautifulSoup
+import requests
 
 @csrf_exempt
 def index(request):
     result = []
+    exist = []
+
+    page = requests.get("https://dhlottery.co.kr/common.do?method=main")
+    lotto_count = int(BeautifulSoup(page.text, "html.parser").find('strong', {'id': 'lottoDrwNo'}).text)
+    recent_num = []
+    for num in range(1, 7):
+        recent_num.append(requests.get(f"https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={lotto_count}").json())
+
+    for num in range(lotto_count, lotto_count-10, -1):
+        data = requests.get(f"https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}").json()
+        for i in range(1, 7):
+            exist.append(data.get(f'drwtNo{i}'))
+
     if request.method == 'POST':
-        if request.POST['howmany']=='' or int(request.POST['howmany'])<0:
+        request_num = request.POST['howmany']
+        if request_num=='' or int(request_num)<0:
             return render(request, '../templates/mainpage.html', {'result': result})
-        elif int(request.POST['howmany'])>10:
+        elif int(request_num)>10:
             n = 10
         else:
-            n = int(request.POST['howmany'])
+            n = int(request_num)
 
-        lotto_count = int(numlists.objects.aggregate(no=Max('no'))['no'])
-        lotto_count_num = list(map(int, numlists.objects.get(no=lotto_count).content.split()))
-
-        exist = []
-        for drwno in range(lotto_count, lotto_count - 15, -1):
-            exist.extend(list(map(int, numlists.objects.get(no=drwno).content.split())))
-        exist = list(set(exist))
-
-        result = makenum(exist, n, lotto_count_num)
-
+        result = makenum(exist, n, recent_num)
     return render(request, '../templates/mainpage.html', {'result':result})
